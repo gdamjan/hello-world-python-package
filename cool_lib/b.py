fe8db35bb258c22a8d7985d9ba5fcc4a816449ef
Дамјан Георгиевски <gdamjan@gmail.com>
2022-03-29 12:33:34 +0200

    example: load resource data from package

--- cool_lib/b.py
+++ cool_lib/b.py
@@ -1,3 +1,7 @@
-from __future__ import print_function
+import importlib.resources
+from . import data
 
 print("i am b")
+
+def load_some_data():
+    return importlib.resources.open_text(data, 'some-data.txt')
